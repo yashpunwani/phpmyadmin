50f1623a3810ed20287c826ad94f6cf9bdb271d5
yashpunwani <yashpunwani12340@gmail.com>
2025-09-17 14:09:42 +0530

    fix: lint errors
    Signed-off-by: Yash Punwani <yashpunwani12340@gmail.com>

--- src/Table/Table.php
+++ src/Table/Table.php
@@ -518,8 +518,10 @@ class Table implements Stringable
                         } elseif ($type === 'BINARY' || $type === 'VARBINARY') {
                             $query .= ' DEFAULT 0x' . $defaultValue;
                         } else {
-                            if (str_contains($defaultValue, 'current_timestamp') || 
-                                str_contains($defaultValue, 'CURRENT_TIMESTAMP')) {
+                            if (
+                                    str_contains($defaultValue, 'current_timestamp')
+                                    || str_contains($defaultValue, 'CURRENT_TIMESTAMP')
+                                ) {
                                 $query .= ' DEFAULT CURRENT_TIMESTAMP(' . $length . ')';
                             } else {
                                 $query .= ' DEFAULT ' . $dbi->quoteString($defaultValue);
